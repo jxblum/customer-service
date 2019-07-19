3ec35765168d67733873e69e425e61de39dd4624
John Blum <jblum@pivotal.io>
2019-07-19 09:54:49 -0700

    Add CustomerService, ApplicationRunner to persist and then access (lookup) a Customer in Apache Geode, Locally.

--- src/main/java/io/pivotal/crm/customerservice/CustomerServiceApplication.java
+++ src/main/java/io/pivotal/crm/customerservice/CustomerServiceApplication.java
@@ -1,12 +1,52 @@
 package io.pivotal.crm.customerservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.geode.cache.client.ClientRegionShortcut;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+
+import io.pivotal.crm.customerservice.model.Customer;
+import io.pivotal.crm.customerservice.repo.CustomerRepository;
 
 @SpringBootApplication
+@EnableEntityDefinedRegions(clientRegionShortcut = ClientRegionShortcut.LOCAL)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	@SuppressWarnings("unused")
+	ApplicationRunner runner(CustomerRepository customerRepository) {
+
+		return args -> {
+
+			assertThat(customerRepository.count()).isEqualTo(0);
+
+			Customer jonDoe = Customer.newCustomer(1L, "Jon Doe");
+
+			System.err.printf("Saving Customer [%s]...%n", jonDoe);
+
+			jonDoe = customerRepository.save(jonDoe);
+
+			assertThat(jonDoe).isNotNull();
+			assertThat(jonDoe.getId()).isEqualTo(1L);
+			assertThat(jonDoe.getName()).isEqualTo("Jon Doe");
+			assertThat(customerRepository.count()).isEqualTo(1);
+
+			System.err.println("Querying for Customer [SELECT * FROM /Customers WHERE name LIKE '%Doe']...");
+
+			Customer queriedJonDoe = customerRepository.findByNameLike("%Doe");
+
+			assertThat(queriedJonDoe).isEqualTo(jonDoe);
+
+			System.err.printf("Customer was [%s]%n", queriedJonDoe);
+		};
 	}
 }
