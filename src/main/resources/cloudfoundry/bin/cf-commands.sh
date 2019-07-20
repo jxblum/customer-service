36d2d1833a1b8e5257b02f008b82ef6c80c216d7
John Blum <jblum@pivotal.io>
2019-07-19 18:16:20 -0700

    Include cf login commands for PCFDev and PCFOne.

--- src/main/resources/cloudfoundry/bin/cf-commands.sh
+++ src/main/resources/cloudfoundry/bin/cf-commands.sh
@@ -1,4 +1,6 @@
 # !/bin/bash
 # Bash shell script to execute cf CLI commands.
 
+cf login -a https://api.dev.cfdev.sh --skip-ssl-validation
+cf login -a api.run.pcfone.io --sso
 cf cups apacheGeodeService -t "gemfire, cloudcache, database, pivotal" -p '{ "locators": [ "10.99.199.24[10334]" ], "urls": { "gfsh": "https://10.99.199.24:8080/gemfire/v1" }, "users": [{ "password": "admin", "roles": [ "cluster_operator" ], "username": "admin" }] }'
